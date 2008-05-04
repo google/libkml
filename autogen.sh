3ad3a8a6591bced8ef9d1c8d92145bd1322c0407
kml.bent <kml.bent@bf31a577-0547-0410-9073-f3f9481b7c33>
2008-05-04 03:30:57 +0000

    update automake to boost intrusive_ptr

--- autogen.sh
+++ autogen.sh
@@ -2,6 +2,8 @@
 # Note: If you install Xcode 2.5 on OS X 2.4.x it destroys the libtool library.
 # You MUST have Xcode 2.4.x if you're on Tiger.
 
+rm -rf autom4te.cache
+
 PLATFORM=`uname -s`
 echo Platform is $PLATFORM
 
