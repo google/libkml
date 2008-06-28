3f905970f4ca6b9f374fa395380d42385172808e
Michael Ashbridge <mtj.ashbridge@gmail.com>
2008-06-28 01:03:25 +0000

    - fix feature_list iteration - distribute KML example files

--- src/kml/convenience/feature_list.cc
+++ src/kml/convenience/feature_list.cc
@@ -24,6 +24,8 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // This file contains the implementation of the FeatureList class.
+// While STL list is not directly exposed the name FeatureList is as such
+// to suggest list-like performance.
 
 #include "kml/convenience/feature_list.h"
 #include <stdlib.h>
@@ -79,10 +81,14 @@ size_t FeatureList::Save(ContainerPtr container) const {
 
 size_t FeatureList::BboxSplit(const Bbox& bbox, size_t max,
                               FeatureList* output) {
-  if (max == 0) {
+  if (max == 0) {  // "0" has the special meaning of "all".
     max = feature_list_.size();
   }
   size_t count = 0;
+  // See Effective STL, by Scott Meyers, Item 9, page 46.  A while loop is used
+  // instead of for to make explicit the advancement of iter within the loop.
+  // If the item is erased then list.erase() returns the advanced iter,
+  // else we advance it oursevles.
   feature_list_t::iterator iter = feature_list_.begin();
   while (iter != feature_list_.end()) {
     double lat, lon;
@@ -91,13 +97,13 @@ size_t FeatureList::BboxSplit(const Bbox& bbox, size_t max,
       if (output) {
         output->PushBack(*iter);
       }
-      iter = feature_list_.erase(iter);
+      iter = feature_list_.erase(iter);  // list.erase() advances to next.
       ++count;
       if (--max == 0) {  // max guaranteed to be > 0.
         break;
       }
     } else {
-      ++iter;
+      ++iter;  // Not erasing so we advance to next explicitely.
     }
   }
   return count;
@@ -128,6 +134,7 @@ struct CompareFeatures
 
 // Sort the internal list of features based on score.
 void FeatureList::Sort() {
+  // See Effective STL, by Scott Meyers, Item 44, page 192.
   feature_list_.sort(CompareFeatures());
 }
 
