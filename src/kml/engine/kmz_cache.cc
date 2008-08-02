4f5f268138ac4258289aa75a02b7b4a5f29f7b89
kml.bent <kml.bent@bf31a577-0547-0410-9073-f3f9481b7c33>
2008-08-02 03:36:56 +0000

    factor out kmlbase::NetCache

--- src/kml/engine/kmz_cache.cc
+++ src/kml/engine/kmz_cache.cc
@@ -36,31 +36,26 @@ bool KmzCache::FetchUrl(const std::string& kml_url, std::string* content) {
   std::string path_in_kmz;
   bool is_kmz = KmzSplit(kml_url, &fetchable_url, &path_in_kmz);
   if (!is_kmz) {
-    // This is a KMZ file cache only.  Anything else we just fetch directly.
-    return network_fetch_func_(kml_url.c_str(), content);
+    // This not a KMZ file: fetch it through the internal memory file cache.
+    if (kmlbase::MemoryFilePtr file = memory_file_cache_->Fetch(kml_url)) {
+      if (content) {
+        *content = file->get_content();
+      }
+      return true;
+    }
+    return false;  // No such URL.
   }
   // See if this in in the cache.
   if (FetchFromCache(fetchable_url, path_in_kmz, content)) {
     return true;
   }
   // If we get here the URL isn't into a KMZ in the cache, so fetch the KMZ.
-  // NOTE: We're borrowing the users content buffer for a bit.
-  if (!network_fetch_func_(fetchable_url.c_str(),  content)) {
+  const KmzFilePtr kmz_file = Fetch(fetchable_url);
+  if (!kmz_file) {
     return false;  // Network fetch failed, return the bad news.
   }
-  // Fetch of this URL succeeded.  Let's see what we got.
-  if (!KmzFile::IsKmz(*content)) {
-    return false;  // Fail out if this is not actually KMZ content.
-  }
-  // Create a KmzFile for this KMZ data.
-  KmzFilePtr kmz_file = KmzFile::OpenFromString(*content);
-  if (!kmz_file) {
-    return false;  // Fail out on KmlFile creation failure.
-  }
-  // Yes, it opened fine as KMZ so save this KmzFile in the cache using the
-  // network fetchahble URL as the cache key.
-  Save(fetchable_url, kmz_file);
-  // Now proceed to try to dig out the content of the given file within the KMZ.
+  // Network fetch succeeded.  Now proceed to try to dig out the content of the
+  // given file within the KMZ.
   return FetchFromCache(fetchable_url, path_in_kmz, content);
 }
 
@@ -78,58 +73,6 @@ bool KmzCache::FetchFromCache(const std::string& fetchable_url,
     return kmz_file->ReadFile(path_in_kmz.c_str(), content);
   }
   return false;
-}
-
-// TODO: generalize KmzCacheEntry and make these templates on the val type.
-const kmlengine::KmzFilePtr KmzCache::LookUp(const std::string& url) const {
-  KmzFileMap::const_iterator iter = kmz_file_map_.find(url);
-  if (iter == kmz_file_map_.end()) {
-    return NULL;
-  }
-  // iter->first is key, second is val and val is KmzCacheEntry pair whose
-  // first is KmlFilePtr (second is creation time of cache entry).
-  return iter->second.first;
-}
-
-bool KmzCache::Save(const std::string& url,
-                    const kmlengine::KmzFilePtr& kmz_file) {
-  const kmlengine::KmzFilePtr exists = LookUp(url);
-  if (exists) {
-    return false;
-  }
-  if (kmz_file_map_.size() == max_entries_) {
-    RemoveOldest();
-  }
-  KmzCacheEntry kmz_cache_entry = std::make_pair(kmz_file,
-                                                 kmlbase::GetMicroTime());
-  kmz_file_map_[url] = kmz_cache_entry;
-  return true;
-}
-
-bool KmzCache::Delete(const std::string& url) {
-  const kmlengine::KmzFilePtr kmzfile = LookUp(url);
-  if (kmzfile) {
-    kmz_file_map_.erase(url);
-    return true;
-  }
-  return false;
-}
-
-bool KmzCache::RemoveOldest() {
-  if (kmz_file_map_.empty()) {
-    return false;
-  }
-  // Find the entry with the smallest time.
-  KmzFileMap::iterator iter = kmz_file_map_.begin();
-  KmzFileMap::iterator oldest = iter;
-  for (;iter != kmz_file_map_.end(); ++iter) {
-    // STL map iter is a pair<key,val> with val KmzCacheEntry which is a pair
-    if (iter->second.second < oldest->second.second) {
-      oldest = iter;
-    }
-  }
-  kmz_file_map_.erase(oldest);
-  return true;
 }
 
 }  // end namespace kmlengine
--- src/kml/engine/kmz_cache.h
+++ src/kml/engine/kmz_cache.h
@@ -23,43 +23,36 @@
 // OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+// This file contains the KmzCache class declaration.
+
 #ifndef KML_ENGINE_KMZ_CACHE_H__
 #define KML_ENGINE_KMZ_CACHE_H__
 
 #include <map>
 #include <string>
+#include "boost/scoped_ptr.hpp"
+#include "kml/base/memory_file.h"
+#include "kml/base/net_cache.h"
+#include "kml/engine/kml_uri.h"
 #include "kml/engine/kmz_file.h"
 
 namespace kmlengine {
 
-// This is the signature of the function used by KmzCache to fetch the content
-// at the given URL.  It is the responsibility of the implementation to detect
-// 404s and other such errors.  A true return status indicates to KmzCache that
-// the expected data at the given URL was fetched properly.
-typedef bool NetworkFetchFunc(const char* url, std::string* content);
-
-// A KMZ cache entry is a mapping from the URL used to fetch the KMZ file
-// to a pair of the KmzFile representing the local copy of the fetched and
-// opened KMZ file and the absolute time the KMZ was fetched.
-// This is a type used internally to KmzClass whose intended access is via
-// the public methods described below.  As an implementation note this is
-// one map of pairs as opposed to a pair of maps to better enforce consistency.
-// Given that this caches a fairly heavyweight item correctness is placed
-// at a premium in this code.
-// TODO: expiration
-typedef std::pair<kmlengine::KmzFilePtr, double> KmzCacheEntry;
-typedef std::map<const std::string, KmzCacheEntry> KmzFileMap;
-
-class KmzCache {
+// This class is a cache of network-fetched KmzFile's.  The NetFetcher
+// is supplied by the caller to implement application-specific networking.
+// See kmlbase::NetCache for more information.  The intended usage is for
+// a given application to create one KmzCache through which all KML-related
+// fetches occur.
+class KmzCache : public kmlbase::NetCache<KmzFile> {
+  typedef kmlbase::NetCache<kmlbase::MemoryFile> MemoryFileCache;
  public:
-  // The constructor must be supplied with the NetworkFetchFunc and a given
-  // limit on the number of open KmzFile's to cache.  Note that the number
-  // of entries to cache is expected to be less than the maximum number of
-  // open file descriptors for a given system and is thus expected to be
-  // generally fairly small (<100 entries in typical usage).
-  KmzCache(NetworkFetchFunc* network_fetch_func, size_t max_entries)
-      : network_fetch_func_(network_fetch_func),
-        max_entries_(max_entries) {}
+  // This creates a KmzCache to hold up to the given number of KmzFiles.
+  // This same size is used for an internal cache of MemoryFile's of fetched
+  // files which are not KMZ.
+  KmzCache(kmlbase::NetFetcher* net_fetcher_, size_t max_size)
+    : kmlbase::NetCache<KmzFile>(net_fetcher_, max_size) {
+    memory_file_cache_.reset(new MemoryFileCache(net_fetcher_, max_size));
+  }
 
   // This is the main public method to fetch a "KML URL" through the cache.
   // A "KML URL" is anything that may appear in a <href> especially including
@@ -67,10 +60,12 @@ class KmzCache {
   // The engine::KmzSplit method is used to detect and save the two parts
   // of a "KML URL" if it is this variety of KMZ file access.  If the
   // KmzCache has no entry for the given KMZ file it is fetched using the
-  // NetworkFetchFunc provided in the constructor.  If the KmzCache does have
+  // NetFetcher provided in the constructuro.  If the KmzCache does have
   // this KMZ cached then this simply accesses the internal KmzFile for this
   // KMZ and reads the content.  This returns true if the KMZ was fetched
   // properly and/or the file within the KMZ was found and read properly.
+  // Finally, if the URL is not to or through a KMZ the file is fetched
+  // and cached in the internal MemoryFileCache.
   bool FetchUrl(const std::string& kml_url, std::string* content);
 
   // If the given "KML URL" is to a KMZ file within the cache then the content
@@ -83,35 +78,8 @@ class KmzCache {
                       const std::string& path_in_kmz,
                       std::string* content) const;
 
-  // This returns the KmlFile for the given "KML URL" or NULL if the cache
-  // has no entry for this URL.  Note that this is the network fetchable URL
-  // such as that returned by the KmzSplit() function.
-  const kmlengine::KmzFilePtr LookUp(const std::string& fetchable_url) const;
-
-  // This stores the given KmzFile into the cache for the given URL.
-  // False is returned and no internal state is altered if an entry already
-  // exists for this URL.  Use Delete() explicitly to remove an entry.
-  // Saving a new item _may_ result in deleting the oldest item in the cache.
-  bool Save(const std::string& fetchable_url,
-            const kmlengine::KmzFilePtr& kmz_file);
-
-  // This removes the KmzFile at the given URL from the cache.  This returns
-  // if an entry was found and deleted and false otherwise.
-  bool Delete(const std::string& fetchable_url);
-
-  // Remove the oldest entry in the cache and return true.  False is returned
-  // if the cache was empty.
-  bool RemoveOldest();
-
-  // This returns the number of entries (URL -> KMZ mappings) in the cache.
-  size_t Size() const {
-    return kmz_file_map_.size();
-  }
-
  private:
-  NetworkFetchFunc* network_fetch_func_;
-  KmzFileMap kmz_file_map_;
-  size_t max_entries_;
+  boost::scoped_ptr<MemoryFileCache> memory_file_cache_;
 };
 
 }  // end namespace kmlengine
