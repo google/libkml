ec98e5b8cfeee9266adb41b4fb79278bb39e79a3
Michael Ashbridge <mtj.ashbridge@gmail.com>
2008-10-25 19:46:49 +0000

    update tests to use googletest

--- src/kml/regionator/regionator_test.cc
+++ src/kml/regionator/regionator_test.cc
@@ -35,7 +35,7 @@
 #include "kml/regionator/region_handler.h"
 #include "kml/regionator/regionator_qid.h"
 #include "kml/regionator/regionator_util.h"
-#include "kml/base/unit_test.h"
+#include "gtest/gtest.h"
 
 namespace kmlregionator {
 
@@ -96,30 +96,14 @@ public:
 
 
 // This class is the unit test fixture for the KmlHandler class.
-class RegionatorTest : public CPPUNIT_NS::TestFixture {
-  CPPUNIT_TEST_SUITE(RegionatorTest);
-  CPPUNIT_TEST(TwoLevelPointRegionatorTest);
-  CPPUNIT_TEST(FourLevelPointRegionatorTest);
-  CPPUNIT_TEST_SUITE_END();
-
- public:
-  // Called before each test.
-  void setUp() {
-  }
-
-  // Called after each test.
-  void tearDown() {
-  }
-
+class RegionatorTest : public testing::Test {
  protected:
   kml_file_map_t kml_file_map_;
   void TwoLevelPointRegionatorTest();
   void FourLevelPointRegionatorTest();
 };
 
-CPPUNIT_TEST_SUITE_REGISTRATION(RegionatorTest);
-
-void RegionatorTest::TwoLevelPointRegionatorTest() {
+TEST_F(RegionatorTest, TwoLevelPointRegionatorTest) {
   // This RegionHandler saves each KML file out to a map.
   PointRegionHandler depth2(2, &kml_file_map_);
   Regionator rtor(depth2, kmlconvenience::CreateRegion2d(10,0,10,0,128,-1));
@@ -127,49 +111,49 @@ void RegionatorTest::TwoLevelPointRegionatorTest() {
   rtor.Regionate(NULL);
 
   // A 2 level RbNL hierarchy has one root and 4 children.
-  CPPUNIT_ASSERT(5 == kml_file_map_.size());
+  ASSERT_EQ(static_cast<size_t>(5), kml_file_map_.size());
 
   // Verify that the 5 files have the expected name and each has <kml> as root.
   // This essentially a test of Regionator::RegionFilename().
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_kml, kml_file_map_["1.kml"]->Type());
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_kml, kml_file_map_["2.kml"]->Type());
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_kml, kml_file_map_["3.kml"]->Type());
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_kml, kml_file_map_["4.kml"]->Type());
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_kml, kml_file_map_["5.kml"]->Type());
+  ASSERT_EQ(kmldom::Type_kml, kml_file_map_["1.kml"]->Type());
+  ASSERT_EQ(kmldom::Type_kml, kml_file_map_["2.kml"]->Type());
+  ASSERT_EQ(kmldom::Type_kml, kml_file_map_["3.kml"]->Type());
+  ASSERT_EQ(kmldom::Type_kml, kml_file_map_["4.kml"]->Type());
+  ASSERT_EQ(kmldom::Type_kml, kml_file_map_["5.kml"]->Type());
 
   // Each KML file has a Document whose name is the region qid.
   // This is essentially a test of Regionator::_Recurse().
-  CPPUNIT_ASSERT_EQUAL(std::string("q0"),
+  ASSERT_EQ(std::string("q0"),
                        kml_file_map_["1.kml"]->get_feature()->get_name());
-  CPPUNIT_ASSERT_EQUAL(std::string("q00"),
+  ASSERT_EQ(std::string("q00"),
                        kml_file_map_["2.kml"]->get_feature()->get_name());
-  CPPUNIT_ASSERT_EQUAL(std::string("q01"),
+  ASSERT_EQ(std::string("q01"),
                        kml_file_map_["3.kml"]->get_feature()->get_name());
-  CPPUNIT_ASSERT_EQUAL(std::string("q02"),
+  ASSERT_EQ(std::string("q02"),
                        kml_file_map_["4.kml"]->get_feature()->get_name());
-  CPPUNIT_ASSERT_EQUAL(std::string("q03"),
+  ASSERT_EQ(std::string("q03"),
                        kml_file_map_["5.kml"]->get_feature()->get_name());
 
   // The Document in the root KML has 5 features.
   DocumentPtr document1 =
       kmldom::AsDocument(kml_file_map_["1.kml"]->get_feature());
-  CPPUNIT_ASSERT(document1);
-  CPPUNIT_ASSERT(5 == document1->get_feature_array_size()); 
+  ASSERT_TRUE(document1);
+  ASSERT_EQ(static_cast<size_t>(5), document1->get_feature_array_size()); 
   // The 5 features are 4 x NetworkLinks + the Folder returned by 
   // PointRegionHandler's GetFeature
   NetworkLinkPtr nw = kmldom::AsNetworkLink(document1->get_feature_array_at(0));
   NetworkLinkPtr ne = kmldom::AsNetworkLink(document1->get_feature_array_at(1));
   NetworkLinkPtr sw = kmldom::AsNetworkLink(document1->get_feature_array_at(2));
   NetworkLinkPtr se = kmldom::AsNetworkLink(document1->get_feature_array_at(3));
-  CPPUNIT_ASSERT_EQUAL(std::string("2.kml"), nw->get_link()->get_href());
-  CPPUNIT_ASSERT_EQUAL(std::string("3.kml"), ne->get_link()->get_href());
-  CPPUNIT_ASSERT_EQUAL(std::string("4.kml"), sw->get_link()->get_href());
-  //CPPUNIT_ASSERT_EQUAL(std::string("5.kml"), se->get_link()->get_href());
-  CPPUNIT_ASSERT_EQUAL(kmldom::Type_Folder,
-                       document1->get_feature_array_at(4)->Type());
+  ASSERT_EQ(std::string("2.kml"), nw->get_link()->get_href());
+  ASSERT_EQ(std::string("3.kml"), ne->get_link()->get_href());
+  ASSERT_EQ(std::string("4.kml"), sw->get_link()->get_href());
+  //ASSERT_EQ(std::string("5.kml"), se->get_link()->get_href());
+  ASSERT_EQ(kmldom::Type_Folder,
+            document1->get_feature_array_at(4)->Type());
 }
 
-void RegionatorTest::FourLevelPointRegionatorTest() {
+TEST_F(RegionatorTest, FourLevelPointRegionatorTest) {
   // This RegionHandler saves each KML file out to a map.
   PointRegionHandler depth4(4, &kml_file_map_);
   Regionator rtor(depth4, kmlconvenience::CreateRegion2d(10,0,10,0,128,-1));
@@ -179,9 +163,12 @@ void RegionatorTest::FourLevelPointRegionatorTest() {
   // A 4 level RbNL hierarchy has one root, 4 children, 16 grand-children,
   // and 64 great-grand-children.  This is partly particular to
   // PointRegionHandler as it always creates 4 children if it creates any.
-  CPPUNIT_ASSERT(85 == kml_file_map_.size());
+  ASSERT_EQ(static_cast<size_t>(85), kml_file_map_.size());
 }
 
 }  // end namespace kmlregionator
 
-TEST_MAIN
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
