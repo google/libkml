3339f242e05f2f7f4bb8e2b6baa585b7c6463133
Michael Ashbridge <mtj.ashbridge@gmail.com>
2008-11-07 00:45:38 +0000

    - move all visual studio files under one directory - build libkmlbase tests on windows - resultant makefile cleanup

---
+++ msvc/tests/run_tests.bat
@@ -0,0 +1,25 @@
+@echo off
+
+rem First build the debug tests in Visual Studio, then run this script from
+rem this directory to execute all tests.
+rem
+rem TODO: disabled tests:
+rem TODO: kmlbase_date_time_test
+rem TODO: kmlbase_file_test
+
+if not exist Debug\libkmlbase_attributes_test.exe (
+  echo No debug tests exist
+  goto end
+)
+
+call exec_tests Debug
+
+rem If the release tests have been built, run them too.
+if not exist Release\libkmlbase_attributes_test.exe (
+  echo No release tests exist
+  goto end
+)
+call exec_tests Release
+
+:end
+
