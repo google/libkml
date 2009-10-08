17fc21c81f64d6af1d7ccf728347cdb68603021b
kml.mashbridge <kml.mashbridge@bf31a577-0547-0410-9073-f3f9481b7c33>
2009-10-08 21:55:20 +0000

    - move init of uriparser to correct location

--- third_party/uriparser-0.7.1/lib/UriParse.c
+++ third_party/uriparser-0.7.1/lib/UriParse.c
@@ -2173,10 +2173,10 @@ UriBool URI_FUNC(_TESTING_ONLY_ParseIpSix)(const URI_CHAR * text) {
 	URI_TYPE(Uri) uri;
 	URI_TYPE(ParserState) parser;
         
-        parser.uri = 0;  /* Initialize to 0 to avoid compiler warning. */
-
 	const URI_CHAR * const afterIpSix = text + URI_STRLEN(text);
 	const URI_CHAR * res;
+
+        parser.uri = 0;  /* Initialize to 0 to avoid compiler warning. */
 
 	URI_FUNC(ResetParserState)(&parser);
 	URI_FUNC(ResetUri)(&uri);
