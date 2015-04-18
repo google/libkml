294c1f3c1da548f90f6547f613895c11abcc1df1
Rashad <mohammedrashadkm@gmail.com>
2015-04-18 14:05:29 +0200

    fix uninitialized variables

--- src/kml/convenience/csv_parser.cc
+++ src/kml/convenience/csv_parser.cc
@@ -72,15 +72,18 @@ bool CsvParser::ParseCsv(kmlbase::CsvSplitter* csv_splitter,
 // private
 CsvParser::CsvParser(kmlbase::CsvSplitter* csv_splitter,
                      CsvParserHandler* csv_parser_handler)
-  : csv_splitter_(csv_splitter),
-    csv_parser_handler_(csv_parser_handler),
-    name_col_(npos),
-    description_col_(npos),
-    lat_col_(npos),
-    lon_col_(npos),
-    feature_id_(npos),
-    style_id_(npos),
-    style_url_base_(kDefaultStyleUrlBase) {
+  : csv_splitter_(csv_splitter)
+  , csv_parser_handler_(csv_parser_handler)
+  , schema_size_(0)
+  , name_col_(npos)
+  , description_col_(npos)
+  , lat_col_(npos)
+  , lon_col_(npos)
+  , feature_id_(npos)
+  , style_id_(npos)
+  , style_url_base_(kDefaultStyleUrlBase)
+  , kml_factory_(0)
+{
 }
 
 // private
