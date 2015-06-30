9f9ef84970b05bbe88e8f26a64334b3e6ab54834
Rashad M <mohammedrashadkm@gmail.com>
2015-07-01 00:28:19 +0200

    update helper macro with named args

--- cmake/LibKMLHelper.cmake
+++ cmake/LibKMLHelper.cmake
@@ -1,38 +1,45 @@
-macro(build_target _target _srcs _dep_libs)
-add_library(${_target} ${_srcs})
-foreach(dep_lib ${_dep_libs})
-add_dependencies(${_target} ${dep_lib})
-endforeach()
-set(_link_libs ${ARGN})
-target_link_libraries(${_target} ${_dep_libs} ${_link_libs})
-if(VERSION_STRING)
-  set_target_properties(${_target} PROPERTIES
-    VERSION   "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
-    SOVERSION "${VERSION_MAJOR}.${VERSION_MINOR}")
-endif()
-endmacro()
+macro(build_target)
+  cmake_parse_arguments(LIB  "" "NAME" "SRCS;INCS;LINKS;DEPENDS" ${ARGN} )
+  add_library(${LIB_NAME} ${LIB_SRCS})
+  foreach(LIB_DEPEND ${LIB_DEPENDS})
+    add_dependencies(${LIB_NAME} ${LIB_DEPEND})
+  endforeach()
+  target_link_libraries(${LIB_NAME} ${LIB_DEPENDS} ${LIB_LINKS})
+  if(VERSION_STRING)
+    set_target_properties(${LIB_NAME} PROPERTIES
+      VERSION   "${VERSION_MAJOR}.${VERSION_MINOR}.${VERSION_PATCH}"
+      SOVERSION "${VERSION_MAJOR}.${VERSION_MINOR}")
+  endif()
+  set(kmlbase__INCLUDE_DIR "dd/ddfg")
+  string(LENGTH ${LIB_NAME} ${LIB_NAME}_LEN)
+  MATH(EXPR ${LIB_NAME}_END "${${LIB_NAME}_LEN} - 3")
+  string(SUBSTRING ${LIB_NAME} 3 ${${LIB_NAME}_END} ${LIB_NAME}_INCLUDE_DIR)
+  install(
+    FILES ${LIB_INCS}
+    DESTINATION include/${${LIB_NAME}_INCLUDE_DIR})
+
+  install_target(${LIB_NAME})
+  
+endmacro(build_target)
 
 macro(install_target _target)
   install(TARGETS ${_target}
-    RUNTIME DESTINATION bin COMPONENT RuntimeLibraries
-    LIBRARY DESTINATION lib COMPONENT RuntimeLibraries
-    ARCHIVE DESTINATION lib COMPONENT Development)
+    RUNTIME DESTINATION bin
+    LIBRARY DESTINATION lib
+    ARCHIVE DESTINATION lib)
+endmacro(install_target)
 
-endmacro()
+function(build_test)
+  cmake_parse_arguments(TEST  "" "GROUP;NAME" "LINKS;DEPENDS" ${ARGN} )
+  add_executable(test_${TEST_NAME} ${TEST_NAME}_test.cc)
+  add_dependencies(test_${TEST_NAME} ${TEST_DEPENDS})
+  target_link_libraries(test_${TEST_NAME} ${TEST_DEPENDS} ${TEST_LINKS} ${GTEST_LIBRARY})
+  add_test(${TEST_GROUP}_${TEST_NAME} ${CMAKE_BINARY_DIR}/bin/test_${TEST_NAME})
+endfunction(build_test)
 
-function(build_test _prefix _target _libs)
- set(_src ${_target}_test.cc)
-set(_extra_libs ${ARGN})
- add_executable(test_${_target} ${_src})
- add_dependencies(test_${_target} ${_libs} ${_extra_libs})
- target_link_libraries(test_${_target} ${_libs} ${_extra_libs} ${GTEST_LIBRARY})
- add_test(${_prefix}_${_target} ${CMAKE_BINARY_DIR}/bin/test_${_target})
-endfunction()
-
-function(build_example _target _libs)
- set(_src ${_target}.cc)
-set(_extra_libs ${ARGN})
- add_executable(example_${_target} ${_src})
- add_dependencies(example_${_target} ${_libs} ${_extra_libs})
- target_link_libraries(example_${_target} ${_libs} ${_extra_libs})
-endfunction()
+function(build_example)
+  cmake_parse_arguments(EXAMPLE  "" "NAME" "LINKS;DEPENDS" ${ARGN} )
+  add_executable(example_${EXAMPLE_NAME} ${EXAMPLE_NAME}.cc)
+  add_dependencies(example_${EXAMPLE_NAME} ${EXAMPLE_DEPENDS})
+  target_link_libraries(example_${EXAMPLE_NAME} ${EXAMPLE_LINKS} ${EXAMPLE_DEPENDS})
+endfunction(build_example)
