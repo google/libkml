017bcb34aee68b87f0374f720d1059d9a570fd79
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-07-19 21:31:14 +0200

    add external_project expat

--- cmake/External_zlib.cmake
+++ cmake/External_zlib.cmake
@@ -10,7 +10,4 @@ ExternalProject_Add(ZLIB
   -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
   -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS})
 
-include_project_vars("ZLIB" "libz.so")
-
-
-
+include_project_vars(ZLIB "libz.so")
