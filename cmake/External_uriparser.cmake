48173698a2992e595db019af6491215de2b2d4fc
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-07-19 21:48:19 +0200

    fix uriparser md5sum

--- cmake/External_uriparser.cmake
+++ cmake/External_uriparser.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(URIPARSER
   PREFIX URIPARSER
   URL "http://sourceforge.net/projects/uriparser/files/Sources/0.8.2/uriparser-0.8.2.tar.bz2/download"
-  URL_MD5 d41d8cd98f00b204e9800998ecf8427e
+  URL_MD5 c5cf6b3941d887deb7defc2a86c40f1d
   BINARY_DIR ${CMAKE_BINARY_DIR}/URIPARSER/build
   DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
   PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/cmake/UriParser_cmake_lists_txt ${CMAKE_BINARY_DIR}/URIPARSER/src/URIPARSER
