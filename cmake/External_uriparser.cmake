6e3606bb598e77815aba64eba69e0bf08ca782d3
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-07-19 21:45:10 +0200

    fix uriparser md5sum

--- cmake/External_uriparser.cmake
+++ cmake/External_uriparser.cmake
@@ -1,16 +1,15 @@
- ExternalProject_Add(URIPARSER
-   PREFIX URIPARSER
-   URL "http://sourceforge.net/projects/uriparser/files/Sources/0.8.2/uriparser-0.8.2.tar.bz2/download"
-   URL_MD5 c5cf6b3941d887deb7defc2a86c40f1d
-   BINARY_DIR ${CMAKE_BINARY_DIR}/URIPARSER/build
-   DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
-   PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${PATCH_DIR}/uriparser/CMakeLists.txt ${CMAKE_BINARY_DIR}/URIPARSER/src/URIPARSER
-   CMAKE_CACHE_ARGS
-   -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}/URIPARSER
-   -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
-   -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS}
-)
+ExternalProject_Add(URIPARSER
+  PREFIX URIPARSER
+  URL "http://sourceforge.net/projects/uriparser/files/Sources/0.8.2/uriparser-0.8.2.tar.bz2/download"
+  URL_MD5 d41d8cd98f00b204e9800998ecf8427e
+  BINARY_DIR ${CMAKE_BINARY_DIR}/URIPARSER/build
+  DOWNLOAD_DIR ${DOWNLOAD_LOCATION}
+  PATCH_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_SOURCE_DIR}/cmake/UriParser_cmake_lists_txt ${CMAKE_BINARY_DIR}/URIPARSER/src/URIPARSER
+  CMAKE_CACHE_ARGS
+  -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}/URIPARSER
+  -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+  -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS} )
 
- include_project_vars(URIPARSER "liburiparser.so")
+include_project_vars(URIPARSER "liburiparser.so")
 
 
