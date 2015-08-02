e73fa2ca5ccd2afa1cfd8d2f66e7470d4a3b5ac7
Rashad Kanavath <mohammedrashadkm@gmail.com>
2015-08-02 16:38:22 +0200

    download minizip from sourceforge libkml-files

--- cmake/External_boost.cmake
+++ cmake/External_boost.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(BOOST
   PREFIX BOOST
-  URL http://sourceforge.net/projects/boost/files/boost-binaries/1.50.0/boost_1_50_headers.zip/download
+  URL "http://sourceforge.net/projects/boost/files/boost-binaries/1.50.0/boost_1_50_headers.zip/download"
   URL_MD5 1605dc6085cb2dc778ef5ab6c0e59083
   SOURCE_DIR ${INSTALL_DIR}/include/boost
   CONFIGURE_COMMAND ""
--- cmake/External_minizip.cmake
+++ cmake/External_minizip.cmake
@@ -1,9 +1,8 @@
 ExternalProject_Add(MINIZIP
   PREFIX MINIZIP
-  URL "https://docs.google.com/uc?export=download&id=0BzqbMPUw_a8uVHF3S2sxX21vNWM"
+  URL "http://sourceforge.net/projects/libkml-files/files/1.3.0/minizip.tar.gz/download"
   URL_MD5 d5f74eff74e03e497ea60b2c43623416
   BINARY_DIR ${CMAKE_BINARY_DIR}/MINIZIP/build
-  DOWNLOAD_COMMAND ${CMAKE_COMMAND} -P ${CMAKE_BINARY_DIR}/download-minizip.cmake
   DEPENDS ${MINIZIP_DEPENDS}
   CMAKE_CACHE_ARGS
   -DCMAKE_INSTALL_PREFIX:PATH=${INSTALL_DIR}
@@ -11,19 +10,5 @@ ExternalProject_Add(MINIZIP
   -DZLIB_INCLUDE_DIR:PATH=${ZLIB_INCLUDE_DIR}
   -DZLIB_LIBRARY:FILEPATH=${ZLIB_LIBRARY}
   -DBUILD_SHARED_LIBS:BOOL=${BUILD_SHARED_LIBS} )
-
-file(WRITE ${CMAKE_BINARY_DIR}/download-minizip.cmake
-  "
- file(DOWNLOAD
-  \"https://docs.google.com/uc?export=download&id=0BzqbMPUw_a8uVHF3S2sxX21vNWM\"
-  \"${CMAKE_BINARY_DIR}/MINIZIP/src/kml-minizip.tar.gz\"
-  SHOW_PROGRESS
-  EXPECTED_HASH;MD5=d5f74eff74e03e497ea60b2c43623416
-  # no TIMEOUT
-  STATUS status
-  LOG log)
- execute_process(COMMAND \"${CMAKE_COMMAND}\" -E tar xfz \"${CMAKE_BINARY_DIR}/MINIZIP/src/kml-minizip.tar.gz\" -C \"${CMAKE_BINARY_DIR}/MINIZIP/src\")
- file(RENAME \"${CMAKE_BINARY_DIR}/MINIZIP/src/minizip\" \"${CMAKE_BINARY_DIR}/MINIZIP/src/MINIZIP\")
-")
 
 include_project_vars(MINIZIP "libminizip")
